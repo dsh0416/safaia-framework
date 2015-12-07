befdee30936c9bd20fcb010179c9b1fae5744aa2
Delton Ding <dsh0416@gmail.com>
2015-12-07 20:42:40 +0800

    Improve log performance

--- framework/safaia/Log.h
+++ framework/safaia/Log.h
@@ -1,8 +1,3 @@
-#define RESET "\033[0m"
-#define GREEN "\033[32m" // Green
-#define YELLOW "\033[33m" // Yellow
-#define RED "\033[1m\033[31m" // Bold Red
-
 #include <iostream>
 #include <string>
 #include <regex>
@@ -12,6 +7,10 @@
 namespace Safaia{
     class Log{
         int is_stdout = isatty(fileno(stdout));
+        const std::string str_reset = "\033[0m";
+        const std::string str_green = "\033[32m";
+        const std::string str_yellow = "\033[33m";
+        const std::string str_red = "\033[1m\033[31m";
 
     public:
         bool ansi_color = true;
@@ -24,16 +23,23 @@ namespace Safaia{
         }
 
         void info(std::string tag, std::string str){
-            if (level > 2)
-                printf("%s[Info] %s: %s%s\n",(ansi_color && is_stdout) ? GREEN : "", tag.c_str(), str.c_str(), (ansi_color && is_stdout) ? RESET : "");
+            if (level > 2){
+                std::string str_log = (((ansi_color && is_stdout) ? str_green : "") + "[Info] " + tag + ": " + str + ((ansi_color && is_stdout) ? str_reset : ""));
+                printf("%s\n", str_log.c_str());
+            }
+
         }
         void warning(std::string tag, std::string str){
-            if (level > 1)
-                printf("%s[Warning] %s: %s%s\n",(ansi_color && is_stdout) ? YELLOW : "", tag.c_str(), str.c_str(), (ansi_color && is_stdout) ? RESET : "");
+            if (level > 1){
+                std::string str_log = (((ansi_color && is_stdout) ? str_yellow : "") + "[Warning] " + tag + ": " + str + ((ansi_color && is_stdout) ? str_reset : ""));
+                printf("%s\n", str_log.c_str());
+            }
         }
         void error(std::string tag, std::string str){
-            if (level > 0)
-                printf("%s[Error] %s: %s%s\n",(ansi_color && is_stdout) ? RED : "", tag.c_str(), str.c_str(), (ansi_color && is_stdout) ? RESET : "");
+            if (level > 0){
+                std::string str_log = (((ansi_color && is_stdout) ? str_red : "") + "[Error] " + tag + ": " + str + ((ansi_color && is_stdout) ? str_reset : ""));
+                printf("%s\n", str_log.c_str());
+            }
         }
     };
 }
