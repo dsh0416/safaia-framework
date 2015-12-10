fcf7c7c43e0944b8c648cf7791448e0b5ca3d161
Delton Ding <dsh0416@gmail.com>
2015-12-10 21:18:01 +0800

    Request deserialization

--- framework/safaia/Request.h
+++ framework/safaia/Request.h
@@ -20,14 +20,48 @@ namespace Safaia{
         std::string user_agent = "";
 
         std::string body = "";
-
         std::vector<std::string> regex_result;
 
-        // TODO: Finish Request Deserialization
         Req(std::string str){
             std::stringstream ss;
             ss << str;
             ss >> method >> request_url >> protocol;
+
+            std::string ln;
+            getline(ss, ln); // Clean up the \n in the first line
+
+            bool stage = 0; // 0 = Header 1 = Body
+            while (getline(ss, ln)){
+                if (ln == "\r"){
+                    stage = true;
+                    continue;
+                }
+                if(!stage){
+                    std::stringstream tmp_ss;
+                    tmp_ss << ln;
+                    std::string header_name;
+                    std::string header_content;
+                    tmp_ss >> header_name;
+                    getline(tmp_ss, header_content);
+                    header[header_name.substr(0, header_name.length() - 1)] = header_content.substr(1, header_content.length() - 1);
+                    std::cout << header_name << std::endl;
+                }
+                if(stage){
+                    body += ln;
+                    body += "\n";
+                }
+            }
+
+            // Fast headers
+            if(header.find("Accept")!=header.end()) accept = header["Accept"];
+            if(header.find("Accept-Encoding")!=header.end()) accept_encoding = header["Accept-Encoding"];
+            if(header.find("Accept-Language")!=header.end()) accept_language = header["Accept-Language"];
+            if(header.find("Cache-Control")!=header.end()) cache_control = header["Cache-Control"];
+            if(header.find("Cookie")!=header.end()) cookie = header["Cookie"];
+            if(header.find("Host")!=header.end()) host = header["Host"];
+            if(header.find("Referer")!=header.end()) referer = header["Referer"];
+            if(header.find("User-Agent")!=header.end()) user_agent = header["User-Agent"];
+
         }
     };
 }
