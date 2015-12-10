36f241176a97b64ae7c075647ffc1b678819367b
Delton Ding <dsh0416@gmail.com>
2015-12-11 00:05:04 +0800

    Fix travis

--- travis-before.sh
+++ travis-before.sh
@@ -3,7 +3,6 @@ if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
     wget --no-check-certificate http://cmake.org/files/v3.4/cmake-3.4.0-Linux-x86_64.sh -O /tmp/cmake_install.sh
     mkdir -p /tmp/cmake
     sh /tmp/cmake_install.sh --exclude-subdir --prefix=/tmp/cmake
-    export PATH="/tmp/cmake/bin:${PATH}"
     cmake --version
 else
     brew update
