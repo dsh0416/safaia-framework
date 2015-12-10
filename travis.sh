0379e0175aeebbceb1d24f178e6222e26c0c54d3
Delton Ding <dsh0416@gmail.com>
2015-12-10 23:59:03 +0800

    Improves travis performance

--- travis.sh
+++ travis.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env sh
-# Install the latest version of CMake
-if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-    wget --no-check-certificate http://cmake.org/files/v3.4/cmake-3.4.0-Linux-x86_64.sh -O /tmp/cmake_install.sh
-    mkdir -p /tmp/cmake
-    sh /tmp/cmake_install.sh --exclude-subdir --prefix=/tmp/cmake
-    export PATH="/tmp/cmake/bin:${PATH}"
-    cmake --version
-else
-    brew update
-    brew unlink cmake
-    brew install cmake
-fi
-# Start building with CMake
 mkdir build || true
 mkdir build/safaia_framework || true
 cd build/safaia_framework
